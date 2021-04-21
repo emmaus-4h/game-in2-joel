3fbf733871e1444bec84ddd75bc7f70b098a80e1
joelyolo <>
2021-04-21 07:31:54 +0000

    javascript verbeterd

--- script.js
+++ script.js
@@ -197,7 +197,8 @@ var y = 200;
 // zie keycode.info voor meer codes van toetsen
 var KEY_LEFT = 37;
 var KEY_RIGHT = 39;
-
+var KEY_UP = 38;
+var KEY_DOWN = 40;
 function setup() {
   createCanvas(800,450);
   background('blue');
@@ -212,10 +213,10 @@ function draw() {
     x = x + 1;
   }  
   if (keyIsDown(KEY_UP)) {
-    y = y + 1;
+    y = y - 1;
   }
   if (keyIsDown(KEY_DOWN)) {
-    y = y - 1;
+    y = y + 1;
   }
   // teken scherm
   background("yellow"); // wis achtergrond
